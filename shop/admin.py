19d27d785144c4768d934dd4b2e1deb58efdf90e
student <razvan1.gavriliu@gmail.com>
2019-04-22 22:00:21 +0300

    Initial commit

---
+++ shop/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+
+# Register your models here.
+from .models import Category, Product
+
+admin.site.register(Category)
+admin.site.register(Product)
