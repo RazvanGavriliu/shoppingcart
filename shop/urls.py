35b62f600aa6bab5dac9ee4b76d683e26ca830a8
razvan <razvan1.gavriliu@gmail.com>
2019-05-13 20:17:15 +0300

    shoppingcart 2

--- shop/urls.py
+++ shop/urls.py
@@ -3,8 +3,9 @@ from django.urls import path, include
 from django.conf.urls import url
 from . import views
 
-# urlpatterns = [
-#     url(r'', views.product_list, name='product_list'),
-#     url(r'^(?P<categoryid>\d+)/$', views.product_list, name='product_list_by_category'),
-#     url(r'^(?P<id>\d+)/$', views.product_detail, name='product_detail'),
-# ]
+urlpatterns = [
+    url(r'^$', views.category_list, name='categories_obj'),
+    url(r'products', views.product_list, name='products_obj'),
+    # url(r'^(?P<categoryid>\d+)/$', views.product_list, name='product_list_by_category'),
+    # url(r'^(?P<id>\d+)/$', views.product_detail, name='product_detail'),
+]
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+
+from .models import Product
+from .models import Category
+
+
+def product_list(request):
+    if request.method == 'GET':
+        products_obj = Product.objects.all()
+
+    return render(request, 'shop/list_products.html', {'products': products_obj})
+
+
+def category_list(request):
+    if request.method == 'GET':
+        categories_obj = Category.objects.all()
+
+    return render(request, 'shop/list_categories.html', {'categories': categories_obj})
